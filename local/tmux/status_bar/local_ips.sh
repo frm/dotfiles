a319459648ebb0b918ade6303661d4f032cc8620
Fernando Mendes <devfrmendes@gmail.com>
2015-12-29 15:48:09 +0000

    Adds multiple interfaces and hostname scripts to tmux

---
+++ local/tmux/status_bar/local_ips.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SEPARATOR_COLOR="#[fg=white]"
+IP_COLOR="#[fg=yellow]"
+
+ifconfig en0 | awk -v ip="$IP_COLOR" '$1 == "inet" { print ip $2 }' \
+  | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ '$SEPARATOR_COLOR'> /g'
