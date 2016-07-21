e7b5537f7a7a6b7fcf3d071fa56c2f6a643f53f5
Fernando Mendes <devfrmendes@gmail.com>
2016-07-22 00:19:12 +0100

    Removes outdated files

--- tmux/status_bar/local_ips.sh
+++
@@ -1,7 +0,0 @@
-SEPARATOR_COLOR="#[fg=white]"
-IP_COLOR="#[fg=yellow]"
-
-#ifconfig en0 | awk -v ip="$IP_COLOR" '$1 == "inet" { print ip $2 }' \
-#  | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ '$SEPARATOR_COLOR'> /g'
-
-ifconfig en0 | awk -v ip="$IP_COLOR" '$1 == "inet" { print ip $2 }'
--- vimrc
+++
@@ -1,8 +0,0 @@
-set nocompatible    " vim not vi
-
-so ~/.vim/rc/base.vim
-so ~/.vim/rc/plugins.vim
-so ~/.vim/rc/scheme.vim
-so ~/.vim/rc/remaps.vim
-so ~/.vim/rc/functions.vim
-so ~/.vim/rc/filetypes.vim
