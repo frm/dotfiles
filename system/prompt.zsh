e532a92a73b5119b8c05f6960e7be742c84eaa1c
Fernando Mendes <devfrmendes@gmail.com>
2016-08-01 10:33:02 +0100

    Adds geometry theme custom options

--- system/prompt.zsh
+++ system/prompt.zsh
@@ -13,3 +13,10 @@ autoload -Uz compinit && compinit
 BASE16_SCHEME="tomorrow-night"
 BASE16_SHELL="$HOME/.config/base16-shell/scripts/base16-$BASE16_SCHEME.sh"
 [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+
+# Customize geometry
+GEOMETRY_COLOR_GIT_DIRTY=9
+GEOMETRY_COLOR_GIT_BRANCH=6
+GEOMETRY_COLOR_EXIT_VALUE=9
+GEOMETRY_COLOR_DIR=242
+GEOMETRY_SYMBOL_EXIT_VALUE="▲"
