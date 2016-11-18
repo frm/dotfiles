16c4b9f28087cb73fe7f4631076bae9529062b78
Fernando Mendes <devfrmendes@gmail.com>
2016-11-18 23:40:17 +0000

    Update tmux theme

--- tmux/scripts/status_bar/timetrap.sh
+++ tmux/scripts/status_bar/timetrap.sh
@@ -3,10 +3,10 @@
 # https://github.com/naps62/dotfiles/blob/a9b27f3e58a7b22ec270f6b099d3c3e9d1cd6823/bin.local/timetrap-tmux-status
 
 timetrap_now=`t n 2> /dev/null`
-if [[ "$timetrap_now" =~ \*([a-zA-Z]+)* ]]; then
+if [[ "$timetrap_now" =~ \@([a-zA-Z]+)* ]]; then
   project=${BASH_REMATCH[1]}
-  echo "#[fg=green] $project #[default]"
+  echo "#[fg=colour242]$project#[default]"
 else
-  echo "#[fg=red] get to work #[default]"
+  echo "#[fg=red]get to work#[default]"
   exit 1
 fi
