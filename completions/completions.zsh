1a47d68714348a118ce0d4c73bf1c2f3bb0cc613
Fernando Mendes <devfrmendes@gmail.com>
2016-11-20 20:16:30 +0000

    Allow git alias completion

--- completions/completions.zsh
+++ completions/completions.zsh
@@ -3,6 +3,9 @@ setopt completeinword
 
 # Case-insensitive tab completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+#
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
 
 # Use menu selection instead of tab cycle
 zstyle ':completion:*' menu select
@@ -10,7 +13,6 @@ zstyle ':completion:*' menu select
 # list-colors when completing
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
-# custom completions
 source $DOTFILES/completions/tmuxinator.zsh
 
 fpath=($DOTFILES/completions $fpath)
