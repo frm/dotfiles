36e0a795a5d0b1383949eca068f083f027e7d8b0
Fernando Mendes <devfrmendes@gmail.com>
2017-03-22 11:33:30 +0000

    nvm, back to xterm

--- system/env.zsh
+++ system/env.zsh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 export EDITOR='nvim'
 
 # Set TERM to screen-256color to allow neovim to capture <M-BS>
-export TERM=screen-256color
+export TERM=xterm-256color
 
 # Loading global env variables
 source $HOME/.env_global
