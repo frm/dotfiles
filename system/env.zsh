f5df9e2377cbfd988e8d16c7300234a5f498172c
Fernando Mendes <devfrmendes@gmail.com>
2017-04-10 11:01:11 +0100

    Set terminal type to screen-256color

--- system/env.zsh
+++ system/env.zsh
@@ -4,7 +4,8 @@ export LANG=en_US.UTF-8
 export EDITOR='nvim'
 
 # Set TERM to screen-256color to allow neovim to capture <M-BS>
-export TERM=xterm-256color
+# Don't forget to remove the 'screen' if branch in the base16 theme script
+export TERM=screen-256color
 
 # Loading global env variables
 source $HOME/.env_global
