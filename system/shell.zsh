d422d0a177b46d3bfdb51eb3faef92b22eb375c1
Fernando Mendes <devfrmendes@gmail.com>
2017-05-04 18:31:43 +0100

    Fix history format

--- system/shell.zsh
+++ system/shell.zsh
@@ -4,13 +4,10 @@
 setopt interactivecomments
 
 # Prompt settings
-COMPLETION_WAITING_DOTS=true
 ENABLE_CORRECTION=false
 
 # History settings
-
 export HISTFILE="$HOME/.zhistory"
-export HIST_STAMPS="dd.mm.yyyy"
 export HISTSIZE=10000000
 export SAVEHIST=10000000
 
@@ -19,5 +16,7 @@ setopt EXTENDED_HISTORY
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 setopt HIST_REDUCE_BLANKS
+
+alias history="fc -El 1"
 
 unsetopt beep
