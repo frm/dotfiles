025ea90e8fe28f6c5769314dc22d96539bd9e3f2
Fernando Mendes <devfrmendes@gmail.com>
2018-02-14 11:20:44 +0000

    Add gcl and x functions

--- functions/functions.zsh
+++ functions/functions.zsh
@@ -27,3 +27,46 @@ t() {
 
   tmux new -s $SESSION_NAME;
 }
+
+
+# git clone a repo and cd to it
+# This needs to be a function to change the current environment
+alias gcl="git-clone-cd"
+git-clone-cd() {
+  if which hub &> /dev/null; then
+    git="hub"
+  else
+    git="git"
+  fi
+
+  $git clone $@
+
+  repo=$(echo $@ | awk -F / '{ print $NF }' | sed 's/.git//g')
+  cd $repo
+}
+
+x() {
+  if [ -f $1 ]; then
+    case $1 in
+      *.tar.bz2)   tar xvjf $1    ;;
+      *.tar.gz)    tar xvzf $1    ;;
+      *.tar.xz)    tar xvJf $1    ;;
+      *.bz2)       bunzip2 $1     ;;
+      *.rar)       unrar x $1     ;;
+      *.gz)        gunzip $1      ;;
+      *.tar)       tar xvf $1     ;;
+      *.tbz2)      tar xvjf $1    ;;
+      *.tgz)       tar xvzf $1    ;;
+      *.zip)       unzip $1       ;;
+      *.Z)         uncompress $1  ;;
+      *.7z)        7z x $1        ;;
+      *.xz)        unxz $1        ;;
+      *.exe)       cabextract $1  ;;
+      *.ace)       unace $1       ;;
+      *.arj)       unarj $1       ;;
+      *)           echo "'$1': unrecognized file compression" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
