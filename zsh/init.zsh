ba8f924a6680b6d82d3ff542ddf2d602cb92f3e4
Fernando Mendes <devfrmendes@gmail.com>
2018-02-14 10:51:51 +0000

    Fix ZSH history

---
+++ zsh/init.zsh
@@ -0,0 +1,38 @@
+# Shell-specific config options
+
+# Enable comments in shell and history
+setopt interactivecomments
+
+# Prompt settings
+ENABLE_CORRECTION=false
+
+# History settings
+export HISTFILE="$HOME/.zhistory"
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+
+setopt HIST_FIND_NO_DUPS
+setopt EXTENDED_HISTORY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_REDUCE_BLANKS
+
+alias history="fc -El 1"
+
+unsetopt beep
+setopt long_list_jobs
+
+# Pretty less
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # green
+export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # cyan
+export LESS_TERMCAP_me=$(tput sgr0)
+export LESS_TERMCAP_so=$(tput bold; tput setaf 1; tput setab 0) # yellow on blue
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # white
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+export LESS_TERMCAP_mr=$(tput rev)
+export LESS_TERMCAP_mh=$(tput dim)
+export LESS_TERMCAP_ZN=$(tput ssubm)
+export LESS_TERMCAP_ZV=$(tput rsubm)
+export LESS_TERMCAP_ZO=$(tput ssupm)
+export LESS_TERMCAP_ZW=$(tput rsupm)
