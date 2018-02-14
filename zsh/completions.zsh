178b64a9d6cdd7618788da26d741a8f6fa9a460b
Fernando Mendes <devfrmendes@gmail.com>
2018-02-14 11:20:59 +0000

    Improve completion system

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,6 +1,26 @@
 autoload -Uz compinit
+
+# Smarter compinit load
 if [ $(date +'%j') != $(stat -f '%Sm' -t '%j' ~/.zcompdump) ]; then
   compinit
 else
   compinit -C
 fi
+
+# Tab completion from both ends
+setopt completeinword
+
+# Case-insensitive tab completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+
+setopt nocasematch
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
+
+# Use menu selection instead of tab cycle
+zstyle ':completion:*:*:*:*:*' menu select
+
+# list-colors when completing
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
