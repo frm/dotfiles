1425f091c14d612aaa92b4bb1b73677d10e3b021
Fernando Mendes <devfrmendes@gmail.com>
2018-04-25 11:54:20 +0100

    Fix cd to git root

--- functions/aliases.zsh
+++ functions/aliases.zsh
@@ -35,6 +35,5 @@ alias ..2="cd ../.."
 alias ..3="cd ../../.."
 alias ..4="cd ../../../.."
 alias ..5="cd ../../../../.."
-alias ...="cd $(git rev-parse --show-toplevel)"
 
 alias wttr="curl wttr.in/~Braga+Portugal"
