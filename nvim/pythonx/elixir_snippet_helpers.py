bbfb5e84e79a8e06bea2a33ee48a2333955f9f97
Fernando Mendes <devfrmendes@gmail.com>
2018-07-12 19:05:40 +0100

    Add UltiSnips

---
+++ nvim/pythonx/elixir_snippet_helpers.py
@@ -0,0 +1,22 @@
+import os
+
+module_path_blacklist = ["apps", "lib", "test"]
+
+def module_name(path, snip):
+    scrubbed_path = scrub_path(path)
+    mod_name = ".".join([camelize(p) for p in scrubbed_path])
+    snip.rv = mod_name
+
+def scrub_path(path):
+    path_without_ext = os.path.splitext(path)[0]
+    split_path = path_without_ext.split(os.sep)
+
+    scrubbed_path = []
+    for p in split_path:
+        if p not in module_path_blacklist and p not in scrubbed_path:
+            scrubbed_path.append(p)
+
+    return scrubbed_path
+
+def camelize(string):
+    return string.replace("_", " ").title().replace(" ", "")
