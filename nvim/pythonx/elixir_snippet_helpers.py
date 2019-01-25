85e3d3d6805a2325deb9e86803d32fe8cb7645ff
Fernando Mendes <fernando@mendes.codes>
2019-01-25 14:47:34 +0000

    Ignore support directory in elixir snippets

--- nvim/pythonx/elixir_snippet_helpers.py
+++ nvim/pythonx/elixir_snippet_helpers.py
@@ -1,6 +1,6 @@
 import snippet_helpers
 
-module_path_blacklist = ["apps", "lib", "test", "controllers", "views"]
+module_path_blacklist = ["apps", "lib", "test", "controllers", "views", "support"]
 
 def module_name(path, snip):
     snip.rv = snippet_helpers.name_from_file(path, ".", module_path_blacklist)
