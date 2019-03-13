c75aaf0b72a9d387f9af0b6fdbed1222d4673d2b
Fernando Mendes <fernando@mendes.codes>
2019-03-13 19:27:59 +0000

    Fix Ruby module snippet

--- nvim/pythonx/ruby_snippet_helpers.py
+++ nvim/pythonx/ruby_snippet_helpers.py
@@ -10,8 +10,26 @@ class_path_blacklist = [
         "test",
         "lib",
         "test",
-        "views"
+        "views",
+        "services"
 ]
 
 def class_name(path, snip):
     snip.rv = snippet_helpers.name_from_file(path, "::", class_path_blacklist)
+
+def open_module(path, snip):
+    mods = snippet_helpers.name_components_from_file(path, class_path_blacklist)
+
+    for idx, mod in enumerate(mods):
+        snip.rv += ("  " * idx) + "module " + mod + "\n"
+
+    # this is required to correctly position the cursor
+    snip.rv += "  " * len(mods)
+
+def close_module(path, snip):
+    mods = snippet_helpers.name_components_from_file(path, class_path_blacklist)
+    length = len(mods) - 1
+
+    for idx, _ in enumerate(mods):
+        indent_level = length - idx
+        snip.rv += "\n" + ("  " * indent_level) + "end"
