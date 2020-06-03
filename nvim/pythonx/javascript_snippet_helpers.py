1e58a63e2b31447bf0931068387f0a931101221f
Fernando Mendes <fernando@mendes.codes>
2020-06-03 15:36:33 +0100

    Add React snippets

---
+++ nvim/pythonx/javascript_snippet_helpers.py
@@ -0,0 +1,15 @@
+import snippet_helpers
+
+blacklist = [
+    "src",
+    "sections",
+    "pages",
+    "lib",
+    "components",
+    "index"
+]
+
+def component_name(path, snip):
+    path_components = snippet_helpers.scrub_path(path, blacklist)
+    name = ".".join(path_components)
+    snip.rv = name
