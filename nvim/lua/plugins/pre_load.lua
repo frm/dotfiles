512280dc60199facba9a6afa22ca8ba5112822d3
Fernando Mendes <fernando.mendes@remote.com>
2024-01-29 16:59:20 +0000

    Fix UltiSnips mapping not working
    Following an UltiSnips update, the keybindings needed to be configured
    before UltiSnips was loaded.
    
    This change transforms the plugin loading setup into three stages:
    
    - plugins/pre_load.lua  -- all setup needed before plugins are loaded
    - plugins.lua           -- plugin loading
    - plugins/post_load.lua -- all setup needed after plugins are loaded

---
+++ nvim/lua/plugins/pre_load.lua
@@ -0,0 +1,13 @@
+-- LuaFormatter off
+
+-----------------------------------------------------------------
+-- UltiSnips
+-----------------------------------------------------------------
+
+vim.g.UltiSnipsExpandOrJumpTrigger = "<C-j>"
+vim.g.UltiSnipsExpandTrigger = "<C-j>"
+vim.g.UltiSnipsJumpForwardTrigger = "<C-j>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
+vim.g.UltiSnipsSnippetsDir = vim.fn.expand("$HOME/.dotfiles/nvim/UltiSnips")
+
+-- LuaFormatter on
