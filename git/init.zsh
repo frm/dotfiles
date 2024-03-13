38096c2b81affca78a32ea9ae4a67aaaf97c1094
Fernando Mendes <fernando.mendes@remote.com>
2024-03-13 14:30:48 +0000

    Automatically set email and signing key based on local computer settings

--- git/init.zsh
+++ git/init.zsh
@@ -3,3 +3,13 @@ source $DOTFILES/git/completions.zsh
 
 # Load custom git commands
 export PATH="$PATH:$DOTFILES/git/bin"
+
+# Sets up user.email and user.signingkey to values set by env vars.
+#
+# Before using git set GIT_EMAIL and GIT_SIGNING_KEY in
+# system/secrets.init
+export GIT_CONFIG_COUNT=2
+export GIT_CONFIG_KEY_0="user.email"
+export GIT_CONFIG_VALUE_0=$GIT_EMAIL
+export GIT_CONFIG_KEY_1="user.signingkey"
+export GIT_CONFIG_VALUE_1=$GIT_SIGNING_KEY
