541ec0237acd8169b9a8e00869a9fd19be1dad9d
Fernando Mendes <fernando.mendes@river.com>
2025-09-10 18:19:24 +0100

    [nvim]: update ultisnips config

--- nvim/lua/plugins/pre_load.lua
+++ nvim/lua/plugins/pre_load.lua
@@ -8,6 +8,6 @@ vim.g.UltiSnipsExpandOrJumpTrigger = "<C-j>"
 vim.g.UltiSnipsExpandTrigger = "<C-j>"
 vim.g.UltiSnipsJumpForwardTrigger = "<C-j>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
-vim.g.UltiSnipsSnippetsDir = vim.fn.expand("$HOME/.dotfiles/nvim/UltiSnips")
+vim.g.UltiSnipsSnippetDirectories = {vim.fn.expand("$HOME/.dotfiles/nvim/UltiSnips")}
 
 -- LuaFormatter on
