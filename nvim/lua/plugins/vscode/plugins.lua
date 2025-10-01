170f5a453539fb702417d05df563f4585c224360
Fernando Mendes <fernando.mendes@river.com>
2025-10-01 12:57:55 +0100

    [cursor+nvim]: use a fork of projectionist to get around file creation prompts

--- nvim/lua/plugins/vscode/plugins.lua
+++ nvim/lua/plugins/vscode/plugins.lua
@@ -23,6 +23,13 @@ require("lazy").setup({
   -- Functionality
   'tpope/vim-abolish',
   'tpope/vim-surround',
+  -- tpope is still the og but I can't use it with cursor since it doesn't
+  -- handle interaction dialogs well. when an alternate doesn't exist, it hangs
+  -- and I can't create it
+  -- run my own version with a :A*C commands that automatically create the alternate
+  -- remove this once cursor fixes interaction windows input with neovim or tpope
+  -- accepts automatic creation somehow
+  { dir = "~/Developer/frm/vim-projectionist" },
 
   'JoosepAlviste/nvim-ts-context-commentstring',
 
