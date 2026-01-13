a4fa50744e21dbb9d72c9bcd87accb9832c0f6a4
Fernando Mendes <fernando.mendes@river.com>
2026-01-12 21:01:19 -0300

    [nvim]: refactor colours

--- nvim/lua/colours.lua
+++ nvim/lua/colours.lua
@@ -1,20 +1,36 @@
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = "soft"
-vim.g.gruvbox_material_enable_italic = 0
-vim.g.gruvbox_material_disable_italic_comment = 1
+-- dracula
+-- vim.cmd("highlight IlluminatedWordText gui=None guibg=#424450")
+-- vim.cmd("highlight IlluminatedWordRead gui=None guibg=#424450")
+-- vim.cmd("highlight IlluminatedWordWrite gui=None guibg=#424450")
 
-vim.g.everforest_better_performance = 1
-vim.g.everforest_background = "soft"
-vim.g.everforest_enable_italic = 0
-vim.g.everforest_disable_italic_comment = 1
-vim.g.everforest_transparent_background = 1
-vim.g.everforest_current_word = "grey background"
+-- gruvbox
+-- vim.g.gruvbox_material_better_performance = 1
+-- vim.g.gruvbox_material_background = "soft"
+-- vim.g.gruvbox_material_enable_italic = 0
+-- vim.g.gruvbox_material_disable_italic_comment = 1
 
--- enable for auto catpuccin theme
+-- everforest
+-- vim.g.everforest_better_performance = 1
+-- vim.g.everforest_background = "soft"
+-- vim.g.everforest_enable_italic = 0
+-- vim.g.everforest_disable_italic_comment = 1
+-- vim.g.everforest_transparent_background = 1
+-- vim.g.everforest_current_word = "grey background"
+
+-- catpuccin
 -- vim.cmd.colorscheme("catppuccin-" .. os.getenv("CATPPUCCIN_VARIANT"))
+
+-- horizon
+vim.api.nvim_create_autocmd("ColorScheme", {
+callback = function()
+  vim.api.nvim_set_hl(0, "IlluminatedWordText",  { bg = "#2a2d37", underline = false })
+  vim.api.nvim_set_hl(0, "IlluminatedWordRead",  { bg = "#2a2d37", underline = false })
+  vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#2a2d37", underline = false })
+end,
+})
 
 vim.cmd.colorscheme("horizon")
 
@@ -22,15 +38,9 @@ vim.opt.showtabline = 0
 vim.opt.laststatus = 2
 vim.opt.fillchars:append("vert:│")
 
--- Set warning signs
 local signs = {Error = "× ", Warn = " ", Hint = "💡", Info = " "}
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
 end
 
--- Fix vim illuminate
--- bg is set to the same as the dracula theme
--- vim.cmd("highlight IlluminatedWordText gui=None guibg=#424450")
--- vim.cmd("highlight IlluminatedWordRead gui=None guibg=#424450")
--- vim.cmd("highlight IlluminatedWordWrite gui=None guibg=#424450")
