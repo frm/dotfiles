b8323e065441d9c8d386a949050736747ef2bada
Fernando Mendes <fernando.mendes@river.com>
2026-01-28 12:17:54 -0300

    [git]: add checkout with worktree support

--- git/completions.zsh
+++ git/completions.zsh
@@ -1,4 +1,15 @@
-_git-cpr() { _git-checkout }
-_git-mpr() { _git-checkout }
-_git-ppr() { _git-checkout }
-_git-del() { _git-checkout }
+__git_branches() {
+  local branches=(${(f)"$(git branch -a --format='%(refname:short)' 2>/dev/null)"})
+  compadd -a branches
+}
+
+_git_wrapper() {
+  case ${words[2]} in
+    co|cpr|mpr|ppr|del) __git_branches ;;
+    *) _git ;;
+  esac
+}
+
+# wrap around git completions and pass them down to hub
+compdef _git_wrapper hub
+compdef _git_wrapper git
