733c8088abefdc9ea57b9dddf1a60e9311dba4d2
Fernando Mendes <fernando.mendes@river.com>
2026-01-29 14:56:00 -0300

    chore(nvim): refactor plugin structure
    - rename post_load.lua and get rid of pre_load.lua - it's now a single
      plugins/config.lua file
    - move custom modules (memorise, assistant, codex) to
      plugins/custom/
    - refactor plugins/config.lua to not re-require local plugins
    - make memorise have a :Memorise command
    - add codex focus (<leader>cf) and select_model
      (<leader>cm) functions
    - update memorise.txt with Claude & Codex keybindings
      table

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,13 @@
 require("pkgmngr")
 require("base")
 require("keybindings")
-require("memorise").setup()
-require("plugins/pre_load")
 
 if vim.g.vscode then
   require("plugins/cursor/plugins")
-  require("plugins/cursor/post_load")
+  require("plugins/cursor/config")
 else
   require("plugins")
-  require("plugins/post_load")
+  require("plugins/config")
 end
 
 require("colours")
